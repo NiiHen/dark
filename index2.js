309c80634f19de9c34c88b01471c4c9357000bd4
NiiHen <63357664+NiiHen@users.noreply.github.com>
2020-05-03 19:33:17 +0700

    Update index2.js

--- index2.js
+++ index2.js
@@ -11,5 +11,5 @@ $(() => {
 });
 //]]>
 
-$('head').append( $('<link rel="stylesheet" type="text/css" />').attr('href', 'https://cdn.jsdelivr.net/gh/NiiHen/dark@master/index2.css') );
+$('head').append( $('<link rel="stylesheet" type="text/css" />').attr('href', 'https://cdn.jsdelivr.net/gh/NiiHen/dark@master/index3.css') );
 document.write("<div class='theme'><label class='switch'><input id='dark' name='toggle-flip' type='checkbox'/><span class='geser round'/></label></div>");
